d704ecdeb3a6de3b01a62fd1db953ac111ff11f1
Sethu Narayan Senthil <sethunarayansenthil@gmail.com>
2019-09-06 10:39:26 -0400

    first / make gitignore for bat files

---
+++ thing.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+import java.lang.Math;
+
+public class thing {
+	public static void main(String[] args){
+		for(int i = 5; i > 0; i--){
+			 for(int j = i; j > 0 ; j--){
+				 out.print("*");
+			 }
+				 out.println("");
+
+		}
+
+				/*	for(int i = 1; i < 5; i++){
+								 for(int j = 5; j > i ; j--){
+									 if( j%2 == 0){
+							         out.print("*");
+
+							        out.print(j);
+
+
+									 }
+								 }
+							 out.println("");
+
+			}*/
+
+for(int i = 0; i < 5; i++){
+	for(int j = 1; j <= i; j++){
+		out.print(j);
+	}
+	out.println("");
+}
+
+
+
+}
+
+
+}
