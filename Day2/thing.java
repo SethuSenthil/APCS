8cd565ec4e7ee6b1f835bec92e3cf52eff31333d
Sethu Narayan Senthil <sethunarayansenthil@gmail.com>
2019-09-18 10:39:13 -0400

    rhzegrzrgrgr

---
+++ Day2/thing.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+import java.lang.Math;
+
+public class thing {
+	public static void main(String[] args){
+		for(int i = 5; i > 0; i--);
+
+
+}
+}
